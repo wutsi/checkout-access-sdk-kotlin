771d93b500cc830ed67ae2121a5e57b65c98471e
wutsi <herve.tchepannou@wutsi.com>
2022-11-27 14:50:06 -0500

    update api

--- src/main/kotlin/com/wutsi/checkout/access/CheckoutAccessApi.kt
+++ src/main/kotlin/com/wutsi/checkout/access/CheckoutAccessApi.kt
@@ -84,10 +84,6 @@ public interface CheckoutAccessApi {
   @Headers(value=["Content-Type: application/json"])
   public fun updateOrderStatus(@Param("id") id: String, request: UpdateOrderStatusRequest): Unit
 
-  @RequestLine("POST /v1/orders/{id}/balance")
-  @Headers(value=["Content-Type: application/json"])
-  public fun updateOrderBalance(@Param("id") id: String): Unit
-
   @RequestLine("POST /v1/transactions/charge")
   @Headers(value=["Content-Type: application/json"])
   public fun createCharge(request: CreateChargeRequest): CreateChargeResponse
