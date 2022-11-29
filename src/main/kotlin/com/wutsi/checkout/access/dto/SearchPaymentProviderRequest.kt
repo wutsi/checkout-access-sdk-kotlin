f30712780b7a23b4cbc9174eedb1519f66ad254b
wutsi <herve.tchepannou@wutsi.com>
2022-11-29 06:12:46 -0500

    update schema

--- src/main/kotlin/com/wutsi/checkout/access/dto/SearchPaymentProviderRequest.kt
+++ src/main/kotlin/com/wutsi/checkout/access/dto/SearchPaymentProviderRequest.kt
@@ -3,7 +3,7 @@ package com.wutsi.checkout.access.dto
 import kotlin.String
 
 public data class SearchPaymentProviderRequest(
-  public val country: String = "",
+  public val country: String? = null,
   public val number: String? = null,
   public val type: String? = null,
 )
